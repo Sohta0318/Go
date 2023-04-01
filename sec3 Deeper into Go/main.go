7dce8a60f1e682645fa77bedf9ee77386f520b21
SOHTA0318 <nicosohta0318@gmail.com>
2023-04-01 18:19:10 -0400

    lesson 20 type & receive function

--- sec3 Deeper into Go/main.go
+++ sec3 Deeper into Go/main.go
@@ -1,7 +1,11 @@
 package main
 
 func main(){
-cards := newDeck()
-
+cards := deck{"Ace of Diamonds", newCard()}
+cards = append(cards, "Six of Spades")
 cards.print()
+}
+
+func newCard() string {
+return "Five of Diamonds"
 }
