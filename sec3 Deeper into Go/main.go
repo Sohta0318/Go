45dac8ff5acb73904a4870337b86c328105b6a7c
SOHTA0318 <nicosohta0318@gmail.com>
2023-04-01 13:29:19 -0400

    lesson 15 variable

--- sec3 Deeper into Go/main.go
+++ sec3 Deeper into Go/main.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
+// var test string = "Hello World"
+var test string
+
 func main(){
 	// var card string = "Ace of Spades"
 	card := "Ace of Spades"
 	card = "Five of Diamonds"
+
+	test = "Hello World"
 	fmt.Println(card)
+	fmt.Println(test)
 }
