a28765f535ef801d61f64510d3f61762646dd415
SOHTA0318 <nicosohta0318@gmail.com>
2023-04-14 10:31:12 -0400

    lesson 42 struct receiver function

--- sec4 Organizing data with Struct/main.go
+++ sec4 Organizing data with Struct/main.go
@@ -2,26 +2,35 @@ package main
 
 import "fmt"
 
-type contactInfo struct{
-email string
-zipCode int
+type contactInfo struct {
+	email   string
+	zipCode int
 }
 
 type person struct {
 	firstName string
-	lastName string
-	contact contactInfo
+	lastName  string
+	contactInfo
 }
 
-func main(){
-jim :=person {
-	firstName: "jim",
-	lastName: "Party",
-	contact: contactInfo {
-		email: "jim@gmail.com",
-		zipCode: 94000,
-	},
+func main() {
+	jim := person{
+		firstName: "jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	jim.updateName("Jimmy")
+	jim.print()
 }
 
-fmt.Printf("%+v", jim)
+func (p person) updateName(newFirstName string){
+	p.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
