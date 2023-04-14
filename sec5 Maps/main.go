14886564291c97e7ecd6e5d5c1eea03563114857
SOHTA0318 <nicosohta0318@gmail.com>
2023-04-14 11:39:19 -0400

    lesson50 manipulate map

--- sec5 Maps/main.go
+++ sec5 Maps/main.go
@@ -3,10 +3,20 @@ package main
 import "fmt"
 
 func main(){
-	colors := map[string]string{
-		"red": "#ff0000",
-		"green": "#4bf5745",
-	}
+	// colors := map[string]string{
+	// 	"red": "#ff0000",
+	// 	"green": "#4bf5745",
+	// }
+
+	// var colors map[string]string
+
+	colors := make(map[int] string)
+
+	colors[10] = "#fffff"
+
+	delete(colors, 10)
+
+
 
 	fmt.Println(colors)
 }
