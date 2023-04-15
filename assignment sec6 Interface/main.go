906e8b7ba0736d85308809a1f8e3b4c5eaef1cd9
SOHTA0318 <nicosohta0318@gmail.com>
2023-04-15 10:53:26 -0400

    sec6 assignmnet interface

---
+++ assignment sec6 Interface/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type square struct {
+	sideLength float64
+}
+
+type triangle struct {
+	height float64
+	base   float64
+}
+
+func main() {
+	testSquare := square{sideLength: 10}
+	testTriangle := triangle{height: 20, base: 14}
+	printArea(testSquare)
+	printArea(testTriangle)
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
+
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
