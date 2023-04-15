c5c277c88be0968f116898ed031a8b5f0c39d8cf
SOHTA0318 <nicosohta0318@gmail.com>
2023-04-15 10:42:10 -0400

    lesson67 io.Copy

--- sec6 Http requst/main.go
+++ sec6 Http requst/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -13,9 +16,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	bs := make([]byte, 99999)
+	lw := logWriter{}
 
-	resp.Body.Read(bs)
+	io.Copy(lw, resp.Body)
+}
 
+func (logWriter) Write(bs []byte) (int, error) {
 	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many byres:", len(bs))
+	return len(bs), nil
 }
