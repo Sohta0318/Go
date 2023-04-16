9991baf83754579be4a6a08a20287251c8d087f7
SOHTA0318 <nicosohta0318@gmail.com>
2023-04-16 09:58:04 -0400

    lesson72 implement go

--- sec7 Channels & Go Routines/main.go
+++ sec7 Channels & Go Routines/main.go
@@ -9,7 +9,7 @@ func main() {
 	links := []string{"http://google.com", "http://facebook.com", "http://stackoverflow.com", "http://golang.org", "http://amazon.com"}
 
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link)
 	}
 }
 
